18d241767cb03c42431c07868723cf4a01e2f761
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2023-04-21 19:32:29 +0200

    Add navigation to header links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedLink: string = "recipes";
+
+  onNavigate(feature: string) {
+    this.selectedLink = feature;
+  }
+}
