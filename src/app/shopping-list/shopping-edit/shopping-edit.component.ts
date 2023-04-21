691b102b1bd61fbd615a246cdf31750eb32ad1d1
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2023-04-21 21:07:39 +0200

    Add ingredient logic

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
   selector: "app-shopping-edit",
   templateUrl: "./shopping-edit.component.html",
   styleUrls: ["./shopping-edit.component.css"],
 })
-export class ShoppingEditComponent {}
+export class ShoppingEditComponent {
+  @ViewChild("nameInput") ingredientName: ElementRef;
+  @ViewChild("amountInput") ingredientAmount: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
+
+  addIngredient() {
+      const name = this.ingredientName.nativeElement.value
+      const amount = this.ingredientAmount.nativeElement.value
+
+    this.ingredientAdded.emit(new Ingredient(name, amount))
+  }
+}
