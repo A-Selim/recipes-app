7b99c0c7832d6dc305fe00e55019c74f77607ea9
Ahmed Selim <65416364+A-Selim@users.noreply.github.com>
2023-05-02 12:58:48 +0300

    Remove unused code

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from "@angular/core";
 import { Recipe } from "../../recipe.model";
-import { RecipeService } from "../../recipe.service";
 
 @Component({
   selector: "app-recipe-item",
@@ -10,6 +9,4 @@ import { RecipeService } from "../../recipe.service";
 export class RecipeItemComponent {
   @Input() recipeItem: Recipe;
   @Input() id: number;
-
-  constructor(private recipeService: RecipeService) {}
 }
